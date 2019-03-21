10bff7890226b4fd15160baa3c6f7a62d51e1cea
Denerius Moye <dmoye@techlaunch.io>
2019-03-20 20:50:06 -0400

    first commit

---
+++ quiz1.js
@@ -0,0 +1,8 @@
+var logo = "wolf";
+function logoChange(){
+    var image = document.getElementById("wolf")
+    if (logo ==  "wolf"){
+        image.src = "glasses.gif";
+        logo = 'glasses'
+    }
+}
